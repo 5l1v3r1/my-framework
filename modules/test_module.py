eb709b52cbbaf3e67dd9e16b83f7e6e5c0172c80
zevtyardt <44270905+zevtyardt@users.noreply.github.com>
2019-02-26 23:30:49 +0700

    Update test_module.py

--- modules/test_module.py
+++ modules/test_module.py
@@ -1,11 +1,13 @@
+# ini untuk deskripsi modulenya
 # desc: this is hmmmmmmm
 
 def run(string, logging):
     logging.info(string)
 
-# required __init__ function and parameter
-def __init__(string, logging):
+# fungsi ini buat base nya, wajib diisi satu parameter atau lebih
+def __init__(string, logging): # bisa menambahkan parameter logging kalau mau ganti print jadi logging
     """
+    Ini untuk default value nya , format samain kek dibawah
     :> string: default value
     """
     run(string, logging)
