d8f898b2ddf3bace54a3dbb7d2e4d9ed1df5ca34
zevtyardt <apklover76@gmail.com>
2019-02-26 23:25:29 +0700

    update modules

---
+++ modules/test_module.py
@@ -0,0 +1,11 @@
+# desc: this is hmmmmmmm
+
+def run(string, logging):
+    logging.info(string)
+
+# required __init__ function and parameter
+def __init__(string, logging):
+    """
+    :> string: default value
+    """
+    run(string, logging)
