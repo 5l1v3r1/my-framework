3632bdc49d5ef628350f59e5ae98e0556702fa5a
zevtyardt <apklover76@gmail.com>
2019-02-28 17:20:01 +0700

    update zvm.py

--- zvm.py
+++ zvm.py
@@ -7,10 +7,4 @@ coded by zevtyardt @ 18:23:21 /2019-02-16/
 from core import interpreter
 
 if __name__ == '__main__':
-    try:
-        interpreter.run()
-    except KeyboardInterrupt:
-        print ('') # new line
-        logger.error('interrupt by user.. exiting')
-    except Exception as e:
-        logger.critical(str(e))
+     interpreter.run()
