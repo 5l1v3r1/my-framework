044bf45d08144e1d0d9da69ac7770d141187a957
zevtyardt <apklover76@gmail.com>
2019-03-06 22:56:01 +0700

    update core

--- core/formatter.py
+++ core/formatter.py
@@ -34,33 +34,23 @@ conf.read('conf.ini')
 
 class Formatter(object):
     def modules(self, body, head=None):
-        if not head:
-            head = ('num', 'module', 'description')
-        head_lenght = [len(i) for i in head]
-        body_lenght = max([len(i) for i in body])
-
-        len_num = len(str(len(body))) + 1
-        F = '   {0:>%s}   {1:<%s}   {2}' % (
-                          head_lenght[0] if len_num <= head_lenght[0] else len_num,
-                          head_lenght[1] if body_lenght <= head_lenght[1] else body_lenght)
-
-        print ('\n' + F.replace('>', '^').format(*head))
-        print (F.format('=' * int(re.findall(r'{0:>(.*?)}', F)[0]), *['=' * len(i) for i in head][1:]))
-        for num, name in enumerate(body, start=1):
+        F = '  {0:<%s}  {1}' %  max([len(i) for i in body] + [6])
+        print ('\n' + F.format('module', 'description') + \
+               '\n' + F.format('======', '==========='))
+        for name in body:
             desc = re.findall(r'# desc: (.*?)\n', open('modules/{}.py'.format(name)).read())
             if not desc:
                 desc = ['no file description added']
 
-            print (F.format('{}.'.format(num), name, desc[0]))
+            print (F.format(name, desc[0]))
         print ('')
 
     def help(self, **kwargs):
         lenght = max([len(i) for i in kwargs] + [7])
-        F = '   {0:<%s}   {1}' % lenght
+        F = ' {0:<%s}  {1}' % lenght
 
-        print ('\n' + \
-               F.format('option', 'description') + '\n' + \
-               F.format('======', '==========='))
+        print ('\n' + F.format('option', 'description') + \
+               '\n' + F.format('======', '==========='))
         for i in kwargs:
             print (F.format(i, kwargs[i]))
         print ('') # new line
@@ -73,7 +63,7 @@ class Formatter(object):
                        max([len(str(default[i])) for i in default] + [5])]
             if lenght[1] >= 25:
                 lenght[1] = 25
-            F = '   {0:<%s}   {1:<%s}   {2}' % (lenght[0], lenght[1])
+            F = '  {0:<%s}  {1:<%s}  {2}' % (lenght[0], lenght[1])
 
             print ('\n' + \
                    F.format('param', 'value', 'description') + '\n' + \
